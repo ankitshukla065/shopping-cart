24a27f57468be91d11116b302e8e2f5bcbd52c77
ankitshukla065 <ankitshukla065@gmail.com>
2018-02-15 21:43:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomePage from './Component/Pages/HomePage';
+import CartPage from './Component/Pages/CartPage';
+import ProductPage from './Component/Pages/ProductPage';
+import { connect } from 'react-redux';
+import { fetchProducts } from './state/product/action';
+import { addToCart, deleteFromCart } from './state/cart/action'; 
+import { bindActionCreators } from 'redux';
+
+class App extends Component {
+  componentWillMount(){
+    this.props.fetchProducts();
+  }
+
+  addProduct(product, value){
+    if(value > 0)
+      product["units"] = value;
+    else
+      product["units"] = 1;
+    this.props.addToCart(product);
+  }
+
+  removeFromCart(_id){
+      this.props.deleteFromCart(_id);
+  }
+
+  updateCart(id, units){
+    this.props.updateItemUnits(id, units);
+  }
+  
+  render() {
+    return (
+      <BrowserRouter>
+      <div>
+          <Route exact path="/"  render={()=> <HomePage products={this.props.products} 
+                                              cart={this.props.cart} 
+                                              addProduct={this.addProduct.bind(this)} />}/>
+          <Route path="/cart" render={()=> <CartPage cart={this.props.cart} removeFromCart={this.removeFromCart.bind(this)}/>} />
+          <Route path="/product/:productId" render={()=> <ProductPage products={this.props.products} addProduct={this.addProduct.bind(this)} />} />
+          </div>
+      </BrowserRouter>
+    );
+  }
+}
+const mapStateToProps = (state) => ({
+  products: state.product.products,
+  cart: state.cart,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchProducts: bindActionCreators(fetchProducts, dispatch),
+  addToCart: bindActionCreators(addToCart, dispatch),
+  deleteFromCart: bindActionCreators(deleteFromCart, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/lib/product/product.mock.js
@@ -0,0 +1,86 @@
+export default [
+  {
+    "_id": "5a7ebab000186e07ca9ef0b5",
+    "image": "http://placehold.it/1000x400",
+    "name": "consectetur veniam id",
+    "price": 15.63,
+    "description": "est laboris consequat proident minim elit et enim cupidatat aute eu laborum fugiat reprehenderit enim dolor nisi dolor magna id adipisicing sunt et labore sint"
+  },
+  {
+    "_id": "5a7ebab04242e9bd02c851ea",
+    "image": "http://placehold.it/1000x400",
+    "name": "fugiat excepteur cupidatat",
+    "price": 87.52,
+    "description": "exercitation reprehenderit laboris incididunt ullamco sint veniam exercitation consectetur quis pariatur amet esse esse esse irure pariatur tempor eu aliquip consequat incididunt qui fugiat sunt"
+  },
+  {
+    "_id": "5a7ebab0184f02ba44265b01",
+    "image": "http://placehold.it/1000x400",
+    "name": "excepteur esse sint",
+    "price": 92.38,
+    "description": "minim id in dolore proident consequat amet elit quis nostrud consequat qui qui mollit nisi aliqua cillum culpa officia ea cillum veniam incididunt commodo amet"
+  },
+  {
+    "_id": "5a7ebab07504dbde5b5d6d85",
+    "image": "http://placehold.it/1000x400",
+    "name": "ipsum exercitation aute",
+    "price": 83.22,
+    "description": "mollit laboris eu non labore deserunt Lorem consectetur nulla reprehenderit anim proident duis minim voluptate ullamco consectetur Lorem exercitation sit cillum ea ipsum eiusmod labore"
+  },
+  {
+    "_id": "5a7ebab097ace2e46776c80d",
+    "image": "http://placehold.it/1000x400",
+    "name": "consectetur nulla minim",
+    "price": 65.32,
+    "description": "amet exercitation laboris enim commodo ex commodo reprehenderit ullamco do qui fugiat ullamco eu irure non anim ex aliqua est excepteur anim consectetur mollit commodo"
+  },
+  {
+    "_id": "5a7ebab06b0064715ec40255",
+    "image": "http://placehold.it/1000x400",
+    "name": "duis excepteur eiusmod",
+    "price": 11.09,
+    "description": "consectetur magna non deserunt sint commodo id anim ea irure eu irure dolore est pariatur sint consectetur cupidatat aliquip sit ullamco enim qui ex proident"
+  },
+  {
+    "_id": "5a7ebab0b88f92d523f1d39c",
+    "image": "http://placehold.it/1000x400",
+    "name": "Lorem sunt dolor",
+    "price": 20.04,
+    "description": "sit tempor velit voluptate amet veniam nostrud exercitation pariatur do veniam dolore aliquip dolore aliqua minim mollit consectetur est quis commodo dolor cupidatat dolor veniam"
+  },
+  {
+    "_id": "5a7ebab017de5f30b28e251a",
+    "image": "http://placehold.it/1000x400",
+    "name": "magna Lorem mollit",
+    "price": 32.76,
+    "description": "in reprehenderit deserunt adipisicing exercitation exercitation mollit amet amet enim Lorem et Lorem dolor duis pariatur duis anim consectetur anim excepteur sint in tempor cillum"
+  },
+  {
+    "_id": "5a7ebab0f2a08a43d3a6eb19",
+    "image": "http://placehold.it/1000x400",
+    "name": "id et eiusmod",
+    "price": 95.73,
+    "description": "veniam duis nisi eu ea culpa exercitation sit reprehenderit sint Lorem est consequat magna ad cupidatat sint eu mollit quis qui aliqua dolore minim sit"
+  },
+  {
+    "_id": "5a7ebab02540c5f4abac1737",
+    "image": "http://placehold.it/1000x400",
+    "name": "in excepteur ipsum",
+    "price": 72.23,
+    "description": "duis nulla tempor cillum quis velit irure aute cupidatat consectetur laboris consectetur qui nostrud consectetur nostrud exercitation exercitation nostrud sint magna cupidatat sunt sunt ipsum"
+  },
+  {
+    "_id": "5a7ebab084bf9ef6b7f7c636",
+    "image": "http://placehold.it/1000x400",
+    "name": "esse officia incididunt",
+    "price": 78.65,
+    "description": "labore nostrud fugiat occaecat commodo sunt excepteur ex proident non consequat velit irure velit occaecat nostrud est ipsum adipisicing ex esse cupidatat Lorem fugiat incididunt"
+  },
+  {
+    "_id": "5a7ebab0459a6580634c3ce5",
+    "image": "http://placehold.it/1000x400",
+    "name": "aliqua nisi ex",
+    "price": 66.44,
+    "description": "amet sunt occaecat elit adipisicing esse ad consectetur aliquip quis amet sit consequat aliqua anim anim nulla deserunt ut qui nisi sunt culpa dolor irure"
+  }
+];
